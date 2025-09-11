18700d0cec56819afa53f27b30ce28f2e4f2399a
GwendolynAK <106776674+GwendolynAK@users.noreply.github.com>
2025-09-11 17:39:27 -0400

    Create index.js

---
+++ backend/models/index.js
@@ -0,0 +1,10 @@
+// Export all models for easy importing
+export { default as User } from './User.js';
+export { default as Student } from './Student.js';
+export { default as Lecturer } from './Lecturer.js';
+export { default as CourseCatalog } from './CourseCatalog.js';
+export { default as VerificationCode } from './VerificationCode.js';
+export { default as Attendance } from './Attendance.js';
+// AttendanceRecord is handled by Attendance.js
+export { default as CourseEnrollment } from './CourseEnrollment.js';
+export { default as AttendanceSession } from './AttendanceSession.js';
